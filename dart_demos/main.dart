a50bf604c349c056637d648a84af020568cf80cd
gelecekvadisi <gelecekvadisiorg@gmail.com>
2024-01-29 21:00:24 +0300

    Print fonksiyonu ve değişken kullanımı işlendi

---
+++ dart_demos/main.dart
@@ -0,0 +1,18 @@
+void main() {
+  //  Aşağıdaki kod kullanıcıyı selamlar.
+  //  print("Hello World!");
+  
+  print("Merhaba Furkan.");
+  print("Uygulamaya hoşgeldin!");
+
+  print("--------------------");
+  print('Merhaba Furkan. \nUygulamaya hoşgeldin!');
+
+  String kullaniciAdi = "Furkan";
+  int bakiye = 5000;
+
+
+  print("Merhaba Furkan. Bakiyeniz " + bakiye.toString() + "TL 'dir.");
+
+  print('Merhaba Furkan. Bakiyeniz $bakiye TL \'dir.');
+}
