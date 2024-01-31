22833d777b56766e967e6d57f92787cc2c39d49c
gelecekvadisi <gelecekvadisiorg@gmail.com>
2024-01-31 21:03:10 +0300

    değişkenler konusu işlendi

---
+++ ders2/baslangic.dart
@@ -0,0 +1,41 @@
+void main() {
+  print("Hello World!");
+
+  //  degisken_tipi degisken_adi = deger;
+  String kullaniciAdi = "Ahmet";
+  String soyad;
+  soyad = "Bozkurt";
+
+  print("Merhaba " + kullaniciAdi + ".");
+  print("Merhaba $kullaniciAdi. Bakiyeniz 1000 TL 'dir.");
+  print("Merhaba $kullaniciAdi. Bugün hava 25 derecedir.");
+  print("Merhaba $kullaniciAdi. Bakiyeniz 1000 TL 'dir.");
+  print("Merhaba $kullaniciAdi. Bugün hava 25 derecedir.");
+
+  print("Merhaba ${kullaniciAdi}. Bugün hava çok güzel.");
+  print('Merhaba $kullaniciAdi. Bugün hava çok güzel.');
+  print("Merhaba '$kullaniciAdi'. Bugün hava çok güzel.");
+  print('Merhaba "$kullaniciAdi". Bugün hava çok güzel.');
+  print("Merhaba \"$kullaniciAdi\". Bugün hava çok güzel.");
+  print('Merhaba \'$kullaniciAdi\'. Bugün hava çok güzel.');
+  print("---------------------------------------------------");
+
+
+  int sayi1 = 10;
+  int sayi2 = 20;
+  print(sayi1 + sayi2);
+
+  double doubleSayi1 = 10.5;
+  double doubleSayi2 = 50.7;
+  print(doubleSayi1 + doubleSayi2);
+
+  double gno = 3.48;
+
+  bool mezunMu = true;
+  bool erkekMi = true;
+  print("Mezun mu değeri: $mezunMu");
+  // bool cinsiyet = false;
+
+
+
+}
---
+++ ders2/obs.dart
@@ -0,0 +1,18 @@
+void main() {
+  String adSoyad = "Furkan Yağmur";
+  String sehir = "İstanbul";
+
+  int sinifi = 3;
+  int dersSayisi = 7;
+  int yas = 25;
+
+  double gno = 3.24;
+  double sonSinavNotu = 78;
+
+  bool mezunMu = false;
+  bool evliMi = true;
+  bool askerlikYaptiMi = false;
+
+  print(
+      "Merhaba $adSoyad. $sinifi. sınıf öğrencisisiniz. Yaşınız $yas. GNO: $gno. Mezun oldunuz mu: $mezunMu.");
+}
