1830fafcdbe97844771e20239b305c5cf7b48840
gelecekvadisi <gelecekvadisiorg@gmail.com>
2024-03-27 22:37:22 +0300

    Container decaorationImage özelliği ve Row, Column, Expanded, Flexible widgetları işlendi. Kendimize ait widget oluşturuldu.

--- flutter/flutter_application_1/lib/main.dart
+++ flutter/flutter_application_1/lib/main.dart
@@ -1,4 +1,7 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:flutter_application_1/home_page.dart';
 
 void main(List<String> args) {
   runApp(const MyApp());
@@ -29,26 +32,185 @@ class MyApp extends StatelessWidget {
             color: Colors.white,
           ),
         ),
-        body: Container(
-          // color: Colors.green,
-          width: 200,
-          height: 200,
-          margin: const EdgeInsets.all(36),
-          decoration: BoxDecoration(
+        body: HomePage(),
+      ),
+    );
+  }
+
+  Row _buildExpandedFlexible() {
+    return Row(
+      children: [
+        Expanded(
+          flex: 3,
+          child: Container(
+            width: 200,
+            height: 200,
+            color: Colors.red,
+          ),
+        ),
+        Expanded(
+          child: Container(
+            width: 170,
+            height: 200,
             color: Colors.blue,
-            // shape: BoxShape.circle,
-            border: Border.all(
-              color: Colors.red,
-              width: 10,
-              // style: BorderStyle.solid
+          ),
+        ),
+        Expanded(
+          flex: 2,
+          child: Container(
+            width: 200,
+            height: 200,
+            color: Colors.green,
+          ),
+        ),
+        /* Flexible(
+            child: Container(
+              width: 200,
+              height: 200,
+              color: Colors.green,
             ),
-            borderRadius: BorderRadius.circular(50),
-            image: const DecorationImage(
-              fit: BoxFit.cover,
-              image: NetworkImage(
-                  "https://www.mynrma.com.au/-/media/2021-best-cars-ford-mustang-hero-mobile.jpg?h=360&w=640&hash=429e38fbefc48878a54438c21c98f863"),
+          ), */
+      ],
+    );
+  }
+
+  Column _icIceColumnRowKullanimi() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Text(
+              "F",
+              style: TextStyle(fontSize: 36, backgroundColor: Colors.red),
+            ),
+            Text(
+              "L",
+              style: TextStyle(fontSize: 36, backgroundColor: Colors.red),
+            ),
+            Text(
+              "U",
+              style: TextStyle(fontSize: 36, backgroundColor: Colors.red),
+            ),
+            Text(
+              "T",
+              style: TextStyle(fontSize: 36, backgroundColor: Colors.red),
+            ),
+            Text(
+              "T",
+              style: TextStyle(fontSize: 36, backgroundColor: Colors.red),
+            ),
+            Text(
+              "E",
+              style: TextStyle(fontSize: 36, backgroundColor: Colors.red),
+            ),
+            Text(
+              "R",
+              style: TextStyle(fontSize: 36, backgroundColor: Colors.red),
+            ),
+          ],
+        ),
+        Text(
+          "L",
+          style: TextStyle(fontSize: 36, backgroundColor: Colors.red),
+        ),
+        Text(
+          "U",
+          style: TextStyle(fontSize: 36, backgroundColor: Colors.red),
+        ),
+        Text(
+          "T",
+          style: TextStyle(fontSize: 36, backgroundColor: Colors.red),
+        ),
+        Text(
+          "T",
+          style: TextStyle(fontSize: 36, backgroundColor: Colors.red),
+        ),
+        Text(
+          "E",
+          style: TextStyle(fontSize: 36, backgroundColor: Colors.red),
+        ),
+        Text(
+          "R",
+          style: TextStyle(fontSize: 36, backgroundColor: Colors.red),
+        ),
+      ],
+    );
+  }
+
+  Container _buildColumn() {
+    return Container(
+      color: Colors.orange,
+      child: Column(
+        mainAxisSize: MainAxisSize.max,
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Container(
+            color: Colors.red,
+            child: Icon(
+              Icons.access_alarm,
+              size: 180,
             ),
           ),
+          Container(
+            color: Colors.blue,
+            child: Icon(
+              Icons.safety_check,
+              size: 64,
+            ),
+          ),
+          Container(
+            color: Colors.green,
+            child: Icon(
+              Icons.facebook_sharp,
+              size: 64,
+            ),
+          ),
+          // Container(
+          //   color: Colors.brown,
+          //   child: Icon(
+          //     Icons.kayaking,
+          //     size: 64,
+          //   ),
+          // ),
+        ],
+      ),
+    );
+  }
+
+  Center _buildContainerDecorationDemo() {
+    return Center(
+      child: Container(
+        //  color: Colors.green,
+        width: 200,
+        height: 200,
+        // margin: const EdgeInsets.all(36),
+        decoration: BoxDecoration(
+          color: Colors.blue,
+          // shape: BoxShape.circle,
+          border: Border.all(
+            color: Colors.red,
+            width: 10,
+            // style: BorderStyle.solid
+          ),
+          borderRadius: BorderRadius.circular(50),
+          image: const DecorationImage(
+            image: NetworkImage(
+              "https://p1.hiclipart.com/preview/365/352/961/simple-linux-logo-black-and-white-penguin-png-clipart.jpg",
+            ),
+            fit: BoxFit.scaleDown,
+            // fit: BoxFit.scaleDown,
+            repeat: ImageRepeat.repeat,
+          ),
+          boxShadow: [
+            BoxShadow(
+                color: Colors.blue, offset: Offset(10, 15), blurRadius: 25),
+            BoxShadow(
+                color: Colors.green, offset: Offset(-10, -15), blurRadius: 25),
+          ],
         ),
       ),
     );
