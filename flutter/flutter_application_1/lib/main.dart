8e195aae162f30abbb979b5ab911689ed0d91abb
gelecekvadisi <gelecekvadisiorg@gmail.com>
2024-03-27 21:06:55 +0300

    Container işlendi

--- flutter/flutter_application_1/lib/main.dart
+++ flutter/flutter_application_1/lib/main.dart
@@ -29,15 +29,62 @@ class MyApp extends StatelessWidget {
             color: Colors.white,
           ),
         ),
-        body: const Center(
-          child: Text(
-            "Hello World!",
-            style: TextStyle(
+        body: Container(
+          // color: Colors.green,
+          width: 200,
+          height: 200,
+          margin: const EdgeInsets.all(36),
+          decoration: BoxDecoration(
+            color: Colors.blue,
+            // shape: BoxShape.circle,
+            border: Border.all(
               color: Colors.red,
-              fontSize: 48,
-              backgroundColor: Colors.blue,
+              width: 10,
+              // style: BorderStyle.solid
+            ),
+            borderRadius: BorderRadius.circular(50),
+            image: const DecorationImage(
+              fit: BoxFit.cover,
+              image: NetworkImage(
+                  "https://www.mynrma.com.au/-/media/2021-best-cars-ford-mustang-hero-mobile.jpg?h=360&w=640&hash=429e38fbefc48878a54438c21c98f863"),
             ),
           ),
+        ),
+      ),
+    );
+  }
+
+  Container buildNestedContainer() {
+    return Container(
+      height: 200,
+      width: 300,
+      color: Colors.amber,
+      // margin: EdgeInsets.only(left: 50, top: 100),
+      // padding: EdgeInsets.all(50),
+      alignment: Alignment.bottomRight,
+      /* constraints: BoxConstraints(
+          maxHeight: 500,
+          maxWidth: 350,
+          minHeight: 100,
+          minWidth: 200,
+        ), */
+      child: Container(
+        width: 100,
+        height: 100,
+        color: Colors.red,
+        child: const Text("Hello World!" /* , textAlign: TextAlign.right, */),
+      ),
+    );
+  }
+
+  Center buildCenteredText() {
+    return const Center(
+      child: Text(
+        "Hello World!",
+        style: TextStyle(
+          color: Colors.red,
+          fontSize: 48,
+          backgroundColor: Colors.blue,
         ),
       ),
     );
