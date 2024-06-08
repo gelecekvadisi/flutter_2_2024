5866030b42bac27c29e2a4c28da6f5fceabba197
Gelecek Vadisi <gelecekvadisiorg@gmail.com>
2024-06-08 10:31:38 +0300

    ExpansionTile, PageView, PageStorageKey, TabBar ve TabBarView işlendi

--- flutter/flutter_widgets_1/lib/pages/home_page.dart
+++ flutter/flutter_widgets_1/lib/pages/home_page.dart
@@ -12,166 +12,239 @@ class HomePage extends StatefulWidget {
   State<HomePage> createState() => _HomePageState();
 }
 
-class _HomePageState extends State<HomePage> {
+class _HomePageState extends State<HomePage>
+    with SingleTickerProviderStateMixin {
   int selectedPageIndex = 0;
+  late PageController _pageController;
+  late TabController tabController;
 
   List<Widget> pageList = [
-    const CameraScreen(),
-    const FeedScreen(),
-    const SearchScreen(),
-    const ProfileScreen(),
+    const CameraScreen(
+      key: PageStorageKey("camera_page"),
+    ),
+    const FeedScreen(
+      key: PageStorageKey("feed_screen"),
+    ),
+    const SearchScreen(
+      key: PageStorageKey("search_screen"),
+    ),
+    const ProfileScreen(
+      key: PageStorageKey("profile_screen"),
+    ),
   ];
 
   @override
+  void initState() {
+    super.initState();
+    _pageController = PageController(
+      initialPage: selectedPageIndex,
+    );
+    _pageController.addListener(() {
+      selectedPageIndex = _pageController.page?.round() ?? 0;
+      tabController.index = _pageController.page?.round() ?? 0;
+    });
+    tabController = TabController(length: pageList.length, vsync: this);
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text("Ana Sayfa"),
+    return DefaultTabController(
+      length: pageList.length,
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text("Ana Sayfa"),
+          bottom: TabBar(
+            controller: tabController,
+            onTap: (index){
+              _pageController.jumpToPage(index);
+            },
+            tabs: const [
+              Tab(
+                text: "Kamera",
+                icon: Icon(Icons.camera),
+              ),
+              Tab(
+                text: "Ana Sayfa",
+                icon: Icon(Icons.home),
+              ),
+              Tab(
+                text: "Arama",
+                icon: Icon(Icons.search),
+              ),
+              Tab(
+                text: "Profil",
+                icon: Icon(Icons.person),
+              ),
+            ],
+          ),
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {},
+          child: const Icon(Icons.add),
+        ),
+        // body: pageList[selectedPageIndex],
+        body: PageView(
+          controller: _pageController,
+          onPageChanged: (index) {
+            setState(() {});
+          },
+          children: pageList,
+        ),
+        // body: TabBarView(children: pageList, controller: tabController,),
+        drawer: _buildDrawer(),
+        bottomNavigationBar: _buildBottomNavigationBar(),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {},
-        child: const Icon(Icons.add),
-      ),
-      drawer: Drawer(
-        child: Column(
-          children: [
-            UserAccountsDrawerHeader(
-              accountName: const Text("Furkan Yağmur"),
-              accountEmail: const Text("furkan@gelecekvadisi.org"),
-              currentAccountPicture: const CircleAvatar(
+    );
+  }
+
+  BottomNavigationBar _buildBottomNavigationBar() {
+    return BottomNavigationBar(
+        // backgroundColor: Colors.blue,
+        currentIndex: selectedPageIndex,
+        onTap: (index) {
+          _pageController.jumpToPage(index);
+          // _pageController.nextPage(duration: Durations.short3, curve: Curves.bounceIn);
+          // _pageController.previousPage(duration: Duration(milliseconds: 400), curve: Curves.bounceOut);
+          // setState(() {
+          //   selectedPageIndex = index;
+          // });
+        },
+        type: BottomNavigationBarType.shifting,
+        items: const [
+          BottomNavigationBarItem(
+              icon: Icon(Icons.camera),
+              label: "Camera",
+              activeIcon: Icon(Icons.photo_album),
+              backgroundColor: Colors.red,
+              tooltip: "Kamera"),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.home),
+              label: "Home",
+              backgroundColor: Colors.green),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.search),
+            label: "Search",
+            backgroundColor: Colors.purple,
+          ),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.person),
+              label: "Profile",
+              backgroundColor: Colors.green),
+        ]);
+  }
+
+  Drawer _buildDrawer() {
+    return Drawer(
+      child: Column(
+        children: [
+          // DrawerHeader(),
+          UserAccountsDrawerHeader(
+            accountName: const Text("Furkan Yağmur"),
+            accountEmail: const Text("furkan@gelecekvadisi.org"),
+            currentAccountPicture: const CircleAvatar(
+              backgroundImage: NetworkImage(
+                  "https://cdn.pixabay.com/photo/2023/12/15/22/37/mountains-8451480_1280.jpg"),
+            ),
+            otherAccountsPictures: const [
+              CircleAvatar(
                 backgroundImage: NetworkImage(
                     "https://cdn.pixabay.com/photo/2023/12/15/22/37/mountains-8451480_1280.jpg"),
               ),
-              otherAccountsPictures: [
-                const CircleAvatar(
-                  backgroundImage: NetworkImage(
-                      "https://cdn.pixabay.com/photo/2023/12/15/22/37/mountains-8451480_1280.jpg"),
+              CircleAvatar(
+                backgroundImage: NetworkImage(
+                    "https://cdn.pixabay.com/photo/2023/12/15/22/37/mountains-8451480_1280.jpg"),
+              )
+            ],
+            onDetailsPressed: () {},
+          ),
+          Expanded(
+            child: ListView(
+              children: [
+                const ListTile(
+                  title: Text("Ana Sayfa"),
                 ),
-                const CircleAvatar(
-                  backgroundImage: NetworkImage(
-                      "https://cdn.pixabay.com/photo/2023/12/15/22/37/mountains-8451480_1280.jpg"),
-                )
+                const ListTile(
+                  title: Text("Kamera Sayfası"),
+                ),
+                const ListTile(
+                  title: Text("Profil Sayfası"),
+                ),
+                SwitchListTile(
+                    thumbIcon: MaterialStateProperty.resolveWith((states) {
+                      if (states.contains(MaterialState.selected)) {
+                        return const Icon(Icons.sunny);
+                      } else if (states.contains(MaterialState.disabled)) {
+                        return const Icon(Icons.nightlight);
+                      }
+                    }),
+                    title: const Text("Karanlık Tema"),
+                    value: false,
+                    onChanged: (value) {}),
+                const ListTile(
+                  title: Text("Ana Sayfa"),
+                ),
+                const ListTile(
+                  title: Text("Kamera Sayfası"),
+                ),
+                const ListTile(
+                  title: Text("Profil Sayfası"),
+                ),
+                SwitchListTile(
+                    thumbIcon: MaterialStateProperty.resolveWith((states) {
+                      if (states.contains(MaterialState.selected)) {
+                        return const Icon(Icons.sunny);
+                      } else if (states.contains(MaterialState.disabled)) {
+                        return const Icon(Icons.nightlight);
+                      }
+                    }),
+                    title: const Text("Karanlık Tema"),
+                    value: false,
+                    onChanged: (value) {}),
+                const ListTile(
+                  title: Text("Ana Sayfa"),
+                ),
+                const ListTile(
+                  title: Text("Kamera Sayfası"),
+                ),
+                const ListTile(
+                  title: Text("Profil Sayfası"),
+                ),
+                SwitchListTile(
+                    thumbIcon: MaterialStateProperty.resolveWith((states) {
+                      if (states.contains(MaterialState.selected)) {
+                        return const Icon(Icons.sunny);
+                      } else if (states.contains(MaterialState.disabled)) {
+                        return const Icon(Icons.nightlight);
+                      }
+                    }),
+                    title: const Text("Karanlık Tema"),
+                    value: false,
+                    onChanged: (value) {}),
+                const ListTile(
+                  title: Text("Ana Sayfa"),
+                ),
+                const ListTile(
+                  title: Text("Kamera Sayfası"),
+                ),
+                const ListTile(
+                  title: Text("Profil Sayfası"),
+                ),
+                SwitchListTile(
+                    thumbIcon: MaterialStateProperty.resolveWith((states) {
+                      if (states.contains(MaterialState.selected)) {
+                        return const Icon(Icons.sunny);
+                      } else if (states.contains(MaterialState.disabled)) {
+                        return const Icon(Icons.nightlight);
+                      }
+                    }),
+                    title: const Text("Karanlık Tema"),
+                    value: false,
+                    onChanged: (value) {}),
               ],
-              onDetailsPressed: () {},
             ),
-            Expanded(
-              child: ListView(
-                children: [
-                  const ListTile(
-                    title: Text("Ana Sayfa"),
-                  ),
-                  const ListTile(
-                    title: Text("Kamera Sayfası"),
-                  ),
-                  const ListTile(
-                    title: Text("Profil Sayfası"),
-                  ),
-                  SwitchListTile(
-                      thumbIcon: MaterialStateProperty.resolveWith((states) {
-                        if (states.contains(MaterialState.selected)) {
-                          return const Icon(Icons.sunny);
-                        } else if (states.contains(MaterialState.disabled)) {
-                          return const Icon(Icons.nightlight);
-                        }
-                      }),
-                      title: const Text("Karanlık Tema"),
-                      value: false,
-                      onChanged: (value) {}),
-                  const ListTile(
-                    title: Text("Ana Sayfa"),
-                  ),
-                  const ListTile(
-                    title: Text("Kamera Sayfası"),
-                  ),
-                  const ListTile(
-                    title: Text("Profil Sayfası"),
-                  ),
-                  SwitchListTile(
-                      thumbIcon: MaterialStateProperty.resolveWith((states) {
-                        if (states.contains(MaterialState.selected)) {
-                          return const Icon(Icons.sunny);
-                        } else if (states.contains(MaterialState.disabled)) {
-                          return const Icon(Icons.nightlight);
-                        }
-                      }),
-                      title: const Text("Karanlık Tema"),
-                      value: false,
-                      onChanged: (value) {}),
-                  const ListTile(
-                    title: Text("Ana Sayfa"),
-                  ),
-                  const ListTile(
-                    title: Text("Kamera Sayfası"),
-                  ),
-                  const ListTile(
-                    title: Text("Profil Sayfası"),
-                  ),
-                  SwitchListTile(
-                      thumbIcon: MaterialStateProperty.resolveWith((states) {
-                        if (states.contains(MaterialState.selected)) {
-                          return const Icon(Icons.sunny);
-                        } else if (states.contains(MaterialState.disabled)) {
-                          return const Icon(Icons.nightlight);
-                        }
-                      }),
-                      title: const Text("Karanlık Tema"),
-                      value: false,
-                      onChanged: (value) {}),
-                  const ListTile(
-                    title: Text("Ana Sayfa"),
-                  ),
-                  const ListTile(
-                    title: Text("Kamera Sayfası"),
-                  ),
-                  const ListTile(
-                    title: Text("Profil Sayfası"),
-                  ),
-                  SwitchListTile(
-                      thumbIcon: MaterialStateProperty.resolveWith((states) {
-                        if (states.contains(MaterialState.selected)) {
-                          return const Icon(Icons.sunny);
-                        } else if (states.contains(MaterialState.disabled)) {
-                          return const Icon(Icons.nightlight);
-                        }
-                      }),
-                      title: const Text("Karanlık Tema"),
-                      value: false,
-                      onChanged: (value) {}),
-                ],
-              ),
-            ),
-          ],
-        ),
+          ),
+        ],
       ),
-      bottomNavigationBar: BottomNavigationBar(
-          // backgroundColor: Colors.blue,
-          currentIndex: selectedPageIndex,
-          onTap: (index) {
-            setState(() {
-              selectedPageIndex = index;
-            });
-          },
-          type: BottomNavigationBarType.shifting,
-          items: const [
-            BottomNavigationBarItem(
-                icon: Icon(Icons.camera),
-                label: "Camera",
-                activeIcon: Icon(Icons.photo_album),
-                backgroundColor: Colors.red,
-                tooltip: "Kamera"),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.home),
-              label: "Home",
-              backgroundColor: Colors.green
-            ),
-            BottomNavigationBarItem(icon: Icon(Icons.search), label: "Search", backgroundColor: Colors.purple,),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.person),
-              label: "Profile",
-              backgroundColor: Colors.green
-            ),
-          ]),
-      body: pageList[selectedPageIndex],
     );
   }
 }
--- flutter/flutter_widgets_1/lib/pages/search_screen.dart
+++ flutter/flutter_widgets_1/lib/pages/search_screen.dart
@@ -3,13 +3,24 @@ import 'package:flutter/material.dart';
 class SearchScreen extends StatelessWidget {
   const SearchScreen({super.key});
 
+  final String contentText =
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vel auctor ex. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Aliquam auctor accumsan porta. Cras efficitur augue id arcu cursus sollicitudin. Aenean id turpis rutrum mauris placerat mollis. In hac habitasse platea dictumst. Vestibulum dui ipsum, consequat in ligula in, dictum mollis sapien. Sed convallis diam erat, non tristique metus molestie ac. Etiam euismod eu magna ultrices interdum. Nullam non malesuada arcu. Donec mollis augue eu accumsan imperdiet. Nam volutpat dui quis eros rutrum iaculis. Sed sit amet posuere justo, et ultricies augue.";
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      alignment: Alignment.center,
-      color: Colors.purple,
-      child: const Text(
-        "Search Screen"),
+    return ListView.builder(
+      itemCount: 40,
+      itemBuilder: (context, index) {
+        return ExpansionTile(
+          key: PageStorageKey("$index"),
+          title: Text("Tile ${index + 1}"),
+          childrenPadding: EdgeInsets.all(16),
+          shape: RoundedRectangleBorder(),
+          children: [
+            Text("${index + 1}. metin : $contentText"),
+          ],
+        );
+      },
     );
   }
 }
